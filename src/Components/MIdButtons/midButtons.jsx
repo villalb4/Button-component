86a02a036777f2e5f6c772f6994f7e13048e6b2b
villalb4 <villalb4angel@gmail.com>
2022-06-20 17:08:53 -0300

    Update

--- src/Components/MIdButtons/midButtons.jsx
+++ src/Components/MIdButtons/midButtons.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 import {midButtons, midButtons_icon} from "./midButtonsData"
-import s from "./MidButtons.module.css"
+import "./MidButtons.css"
+import cart from "./cart.png"
 
 function MidButtons() {
   return (
-    <div className={s.midButtons_container}>
+    <div className="midButtons_container">
 
-      <div className={s.midButtons_div}>
+      <div className="midButtons_div">
         {midButtons.map((m, i) => (
           <React.Fragment key={i}>
             <div>
-              <span className={s.title}>{m.title}</span>
+              <span className="title">{m.title}</span>
               <div>
-                <button>{m.value}</button>
+                <button className={m.style}>{m.value}</button>
               </div>
             </div>
           </React.Fragment>
         ))}
       </div>
 
-      <div className={s.midButtonsIcon_div}>
+      <div className="midButtonsIcon_div">
         {midButtons_icon.map((m, i) => (
           <React.Fragment key={i}>
             <div>
-              <span className={s.title}>{m.title}</span>
+              <span className="title">{m.title}</span>
               <div>
-                <button>{m.value}</button>
+                <button className={m.style}><img className="cart" src={cart} alt="cart" />{m.value}</button>
               </div>
             </div>
           </React.Fragment>
