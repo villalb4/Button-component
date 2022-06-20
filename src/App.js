70170847382d3b466bf07cf6b3e7280ad5f24904
villalb4 <villalb4angel@gmail.com>
2022-06-19 23:40:07 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css"
+import Buttons from "./Components/Buttons";
+
+function App() {
+  return (
+    <div>
+      <h1>Buttons</h1>
+      <Buttons />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/buttonsData.js
@@ -0,0 +1,44 @@
+const buttons = [
+  {
+    title: "<Button />",
+    hoverTitle: "&:hover, &:focus",
+    value: "Default",
+    style: "Button",
+    hoverStyle: "Button_hover",
+    img: ""
+  },
+  {
+    title: "<Button variant=”outline” />",
+    hoverTitle: "&:hover, &:focus",
+    value: "Default",
+    style: "Button_outline",
+    hoverStyle: "Button_outline_hover",
+    img: ""
+  },
+  {
+    title: "<Button variant=”text” />",
+    hoverTitle: "&:hover, &:focus",
+    value: "Default",
+    style: "Button_text",
+    hoverStyle: "Button_text_hover",
+    img: ""
+  },
+  {
+    title: "<Button disableShadow />",
+    hoverTitle: "&:hover, &:focus",
+    value: "Default",
+    style: "Button_shadow",
+    hoverStyle: "Button_shadow_hover",
+    img: ""
+  },
+  {
+    title: "<Button disabled/>",
+    hoverTitle: "&:hover, &:focus",
+    value: "Default",
+    style: "Button_disabled",
+    hoverStyle: "Button_disableText",
+    img: ""
+  },
+]
+
+export default buttons;
