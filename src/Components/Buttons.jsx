835af2143541d9bbaba14f562bad2b30f3a08c99
villalb4 <villalb4angel@gmail.com>
2022-06-20 17:55:45 -0300

    Buttons Finished

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TopButtons from "./TopButtons/TopButtons"
 import MidButtons from "./MIdButtons/midButtons";
+import BotButtons from "./BotButtons/BotButtons";
 import s from "./Buttons.module.css";
 
 function Buttons() {
@@ -9,6 +10,8 @@ function Buttons() {
       <h1 className={s.h1}>Buttons</h1>
       <TopButtons />
       <MidButtons />
+      <BotButtons />
+      <span className={s.link_icons}>Icons: https://material.io/resources/icons/?style=round</span>
     </div>
   )
 }
