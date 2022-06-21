77ecb0f3df977c8b75e453221ec6e8a3bd587668
villalb4 <villalb4angel@gmail.com>
2022-06-21 01:12:45 -0300

    Finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css"
 import Buttons from "./Components/Buttons";
+import SideBar from "./Components/SideBar/SideBar";
 
 function App() {
   return (
     <div>
+      <SideBar />
       <Buttons />
     </div>
   )
